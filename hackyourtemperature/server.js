8e1bf8d57c1a5168634592c91585c0f24d45646c
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-01-29 18:22:25 +0100

    updated

--- hackyourtemperature/server.js
+++ hackyourtemperature/server.js
@@ -1,16 +1,14 @@
-import express from 'express';
+//import express from 'express';
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-console.log('log before app.get');
-
 app.get('/', (request, response) => {
-    console.log('request', request.url);
-    console.log('starting  request/response cycle');
+
     response.send('hello from backend to frontend!');
 
 });
-console.log('done!');
+
 app.listen(PORT, () => {
     console.log(`The server is running on port ${PORT}`);
 });
